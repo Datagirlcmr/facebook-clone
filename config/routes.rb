8fd5fe8e8c2a9b9276fdaf07085335703dc9023e
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-01-29 12:45:14 -0300

    Add users controller with index action; Add link to create new post on posts index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :comments
   devise_for :users
   resources :posts
+  resources :users, only: [:index, :show]
   root to: "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
