1aec991439b168c2e5b72d374d7e3e35bf61dcf8
Selma Datagirl <datagirlcmr@gmail.com>
2020-01-29 11:10:00 -0600

    constructed test cases for the LIke model

--- spec/likes_unit_test_spec.rb
+++ spec/likes_unit_test_spec.rb
@@ -1,32 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-    context 'Comment creation' do
-      it 'Create a comment without author, and post' do
-        comment = Comment.new(content: 'Content123')
-        expect(comment).not_to be_valid
+RSpec.describe Like, type: :model do
+    context 'Like modelling' do
+      it 'Create like without author' do
+        user = User.create(name: 'Test', birth_date: DateTime.now, gender: 'Female', email: 'test@mail.com',
+                           encrypted_password: 'pass123', password: 'pass123')
+        post = user.posts.create(content: 'heylo')
+        like = post.likes.build(author: nil)
+        expect(like).not_to be_valid
       end
   
-      it 'Create a comment without content, and post' do
+      it 'Create a like without post' do
         user = User.create(name: 'Test', birth_date: DateTime.now, gender: 'Female', email: 'test@mail.com',
                            encrypted_password: 'pass123', password: 'pass123')
-        comment = user.comments.build(content: '')
-        expect(comment).not_to be_valid
-      end
-
-      it 'Create a comment without content, and author' do
-        user = User.create(name: 'Test', birth_date: DateTime.now, gender: 'Female', email: 'test@mail.com',
-                           encrypted_password: 'pass123', password: 'pass123')
-        post = user.posts.create(content: 'content101')
-        comment = post.comments.build(content: '')
-        expect(comment).not_to be_valid
+        like = user.likes.build(post: nil)
+        expect(like).not_to be_valid
       end
   
-      it 'Create a valid comment with content, post and author' do
+      it 'Create a valid like with post and author' do
         user = User.create(name: 'Test', birth_date: DateTime.now, gender: 'Female', email: 'test@mail.com',
                            encrypted_password: 'pass123', password: 'pass123')
-        post = user.posts.create(content: 'content101')
-        comment = post.comments.build(content: 'content101', author_id: user.id)
+        post = user.posts.create(content: 'heylo')
+        like = post.likes.create(author: user)
         expect(post).to be_valid
       end
     end
