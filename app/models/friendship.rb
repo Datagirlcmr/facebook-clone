2c10bcad1740547013337837b63fd390082014d8
Vinicius Campos <vcampos.pitangui@gmail.com>
2020-01-31 18:05:30 -0300

    Fix linter errors

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
-  scope :accepted, -> {where(status: true)}
-  scope :unaccepted, -> {where(status: false)}
+  scope :accepted, -> { where(status: true) }
+  scope :unaccepted, -> { where(status: false) }
   belongs_to :user
   belongs_to :friend, class_name: 'User'
   validates :user, presence: true
