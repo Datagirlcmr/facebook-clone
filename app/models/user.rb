e63428f9c9685610d70596fcae45e31f92c4e929
Selma Datagirl <datagirlcmr@gmail.com>
2020-02-03 15:03:20 -0600

    lint errors fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,16 +28,14 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     user = find_by(email: auth.info.email)
-    unless user
-      user = User.create(name: auth.info.name, password: Devise.friendly_token[0, 20], email: auth.info.email, birth_date: DateTime.now, gender: 'non-binary')
-    end
-    user
+    user ||= User.create(name: auth.info.name, password: Devise.friendly_token[0, 20],
+                         email: auth.info.email, birth_date: DateTime.now, gender: 'non-binary')
   end
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      if data == session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
       end
     end
   end
