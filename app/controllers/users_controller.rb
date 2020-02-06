fc773fedcaaa6f12f6985c7b2ae8de4329ba39e2
Selma Datagirl <datagirlcmr@gmail.com>
2020-02-06 15:39:06 -0600

    worked on mailers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(params[:user])
+ 
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.with(user: @user).welcome_email.deliver_later
+ 
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def index
     @users = User.all
   end
